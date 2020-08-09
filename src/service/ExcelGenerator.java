d7db9d7ca53fb52d1d51b9b78cb0ed8c8ffd6c0a
chrysleremmanuel123 <chrysler.emmanuel@gmail.com>
2020-08-10 00:37:34 +0530

    day6 lab2

--- src/service/ExcelGenerator.java
+++ src/service/ExcelGenerator.java
@@ -1,5 +1,5 @@
 package service;
-/*
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -8,6 +8,10 @@ import java.util.List;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import model.Prograd;
 
@@ -22,14 +26,31 @@ public class ExcelGenerator {
 	public HSSFWorkbook excelGenerate(Prograd prograd, List<Prograd> list) throws IOException {
 		try {
 
-
+			XSSFWorkbook xwb = new XSSFWorkbook();
+			XSSFSheet sheet=xwb.createSheet("Test");
+			for(Prograd listitem:list) {
+				int rowcount = 0;
+				XSSFRow row = sheet.createRow(++rowcount);
+				 int columnCount = 0;
+		                	XSSFCell cell1 = row.createCell(++columnCount);
+		                    cell1.setCellValue(listitem.getName()); 
+		                    XSSFCell cell2 = row.createCell(++columnCount);
+		                    cell2.setCellValue(listitem.getId());
+		                    XSSFCell cell3 = row.createCell(++columnCount);
+		                    cell3.setCellValue(listitem.getRate());
+		                    XSSFCell cell4 = row.createCell(++columnCount);
+		                    cell4.setCellValue(listitem.getComment());
+		                    XSSFCell cell5 = row.createCell(++columnCount);
+		                    cell5.setCellValue(listitem.getRecommend());  
 			
 			// Type your code here
 			
 		
 			}
+			String filename = null;
 			// Do not modify the lines given below
 			 out = new FileOutputStream(filename);
+			HSSFWorkbook hwb = null;
 			hwb.write(out);
 		
 			return hwb;
@@ -44,4 +65,3 @@ public class ExcelGenerator {
 		
 	}
 }
-*/
